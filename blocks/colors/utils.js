9fb6acc348ead4b75af1181b3a83a1074c8c7b3e
Jorge <jorge.costa@developer.pt>
2018-05-02 16:24:41 +0100

    Refactor blocks/PanelColor, to avoid the need for the colorName prop.
    With the creation of a PanelColor in blocks, there is no need to receive colorName anymore as the component already has color context available and can get to colorName from it.
    This change makes the usage of the component easier. Existing core blocks that make use of the component were updated.

--- blocks/colors/utils.js
+++ blocks/colors/utils.js
@@ -23,6 +23,20 @@ export const getColorValue = ( colors, namedColor, customColor ) => {
 	}
 };
 
+/*
+* Provided an array of named colors and a color value returns the color name.
+*
+* @param {Array}   colors      Array of color objects containing the "name" and "color" value as properties.
+* @param {?string} colorValue  A string containing the color value.
+*
+* @return {?string} If colorValue is passed and the value is found in colors it returns the color name for that value.
+*                      Otherwise, undefined is returned.
+*/
+export const getColorName = ( colors, colorValue ) => {
+	const colorObj = find( colors, { color: colorValue } );
+	return colorObj ? colorObj.name : undefined;
+};
+
 /**
  * Returns a function that receives the color value and sets it using the attribute for named colors or for custom colors.
  *
